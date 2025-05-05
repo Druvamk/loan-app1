bd4c0f068a63d7c4050c3a7cd3b18bbcce08ecd5
Druva M K <druvamk1@gmail.com>
2025-05-05 14:32:27 +0530

    Implemented header UI and integrated Material UI library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-  return <div></div>;
-}
+import React from "react";
+import "./App.css";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Switch,
+  Box,
+} from "@mui/material";
 
-export default App;
+export default function LoanCalculatorDashboard() {
+  return (
+    <>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Loan Calculator
+          </Typography>
+          <Box display="flex" alignItems="center" gap={2}>
+            <Button color="inherit">HOME</Button>
+            <Button color="inherit">EXCHANGE RATES (LIVE)</Button>
+            <Button color="inherit">ABOUT</Button>
+            <Button color="inherit">ERROR PAGE</Button>
+            <Switch />
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
